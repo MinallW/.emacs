46329e3e59f8598f2620f533d32ef82739c5c2a9
MinallW <suarezmiguelc23@gmail.com>
2020-02-24 15:24:06 -0500

    Starting my Emacs configuration file, configured with org mode

---
+++ elpa/ruby-test-mode-1.0/ruby-test-mode-autoloads.el
@@ -0,0 +1,31 @@
+;;; ruby-test-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ruby-test-mode" "ruby-test-mode.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from ruby-test-mode.el
+
+(autoload 'ruby-test-mode "ruby-test-mode" "\
+Toggle Ruby-Test minor mode.
+With no argument, this command toggles the mode. Non-null prefix
+argument turns on the mode. Null prefix argument turns off the
+mode.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ruby-test-mode" '("ruby-test-" "find-all" "select")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ruby-test-mode-autoloads.el ends here
